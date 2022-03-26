73097639e73dc4b68939ec595f7f71cd1ab0c37f
Yash Patel <yashpatel200403@gmail.com>
2022-03-26 17:36:23 +0530

    Deploy file in progress

---
+++ hardhat-tuts/contracts/CryptoDevs.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier : MIT
+
+pragma solidity ^0.8.4;
+
+import "@openzepplin/contracts/token/ERC721/extensions/ERC721numerable.sol";
+import "@openzepplin/contracts/access/Ownable.sol";
+import "./IWhitelist";
+
+conract CryptoDevs is ERC721numerable, Ownable {
+
+    string _baseTokenURI;
+    uint256 public _price = 0.01 ether;
+
+    bool public _paused;
+    uint256 publix maxTokenIds= 20;
+    uint256 public tokenIds;
+    IWhitelist whitelist;
+
+    bool publix presaleStarted;
+
+    uint256 public presaleEnded;
+    modifier onlyWhenNotPaused {
+        require(!_paused, "Contract currently paused");
+        _;
+    } 
+
+    constructor (string memory baseUrI, address whitelistContract) ERC721("Crypto Devs", "CD") {
+        _baseTokenURI = baseURI;
+        whitelist = IWhitelist(whitelistContract);
+    }
+
+    function startPresale() public onlyOwner {
+        presaleStarted = true;
+        presaleEnded = block.timestamp + 5 minutes;
+    }
+
+    function presaleMint() public payable onlyWhenNotPaused {
+        require(presaleStarted && block.timestamp < presaleEnded, "Presale is not running.");
+        require(whitelist.whitelistedAddresses(msg.sender), "You are not whitelisted");
+        require(tokenIds < maxtokenIds, "Exceeded maximum Crypto Dev supply");
+        require(msg.value >= _price,"Ether sent is not correct");
+        tokenIds += 1;
+
+        _safeMint(msg.sender,tokenIds);
+    }
+
+    function mint() public payable onlyWhenNotPaused {
+        require(presaleStarted && block.timestamp >= presaleEnded, "Presael has not ended yet");
+        require(tokenIds < maxTokenIds, "Exceed maximum Crypto Devs supply");
+        require(msg.value >= _price,"Ether sent is not correct");
+        tokenIds += 1;
+        _safeMint(msg.sender,tokenIds);
+    }
+
+    function _baseURI() internal view virtual override returns(string memory) {
+        return _baseTokenURI;
+
+    }
+
+    function setPaused(bool val) public onlyOwner {
+        _paused = val;
+
+    }
+
+    function withdraw() public onlyOwner {
+        address _owner = owner() ;
+        uint256 amount =address(this).balance;
+        (bool sent,) = _owner.call{value: amount("")};
+        require(sent, "Failed to send Ether");
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
+}
